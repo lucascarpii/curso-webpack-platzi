6dac6753a6970f0a70f91af879596f4ca2827ef2
lucascarpii <lucascarpii2002@gmail.com>
2022-04-26 15:22:35 -0300

    Copia de archivos con Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtracctPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -33,5 +34,13 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtracctPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
   ],
 };
