aba557a37a265fcc6f5524e3ec49d61756e85e67
lucascarpii <lucascarpii2002@gmail.com>
2022-05-02 12:24:02 -0300

    File-loader no funciona con webpack5 asi q deje el import de las fuentes

--- webpack.config.js
+++ webpack.config.js
@@ -30,20 +30,6 @@ module.exports = {
         test: /\.png/,
         type: "asset/resource",
       },
-      {
-        test: /\.(woff|woff2)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            mimetype: "application/font-woff",
-            name: "[name].[ext]",
-            outputPath: "./assets/fonts/",
-            publicPath: "./assets/fonts/",
-            esModule: false,
-          },
-        },
-      },
     ],
   },
   plugins: [
